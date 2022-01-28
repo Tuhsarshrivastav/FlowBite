030747d0053b804392d0847e6f93f12443a75bb3
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-28 10:20:30 +0530

    added public route

--- client/src/routes/Routing.js
+++ client/src/routes/Routing.js
@@ -2,12 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminLogin from "../screens/auth/AdminLogin";
 import Products from "../screens/dashboard/Products";
 import Private from "./Private";
+import Public from "./Public";
+
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="auth">
-          <Route path="admin-login" element={<AdminLogin />} />
+          <Route
+            path="admin-login"
+            element={
+              <Public>
+                <AdminLogin />
+              </Public>
+            }
+          />
         </Route>
         <Route path="dashboard">
           <Route
