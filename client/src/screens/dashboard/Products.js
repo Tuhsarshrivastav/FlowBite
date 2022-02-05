b586b59d0668d1633b8b9f1b16f895ed7bf811a0
Tuhar Shrivastav <ts3657@gmail.com>
2022-02-05 13:38:46 +0530

    added dashboard content section

--- client/src/screens/dashboard/Products.js
+++ client/src/screens/dashboard/Products.js
@@ -1,8 +1,19 @@
 import React from "react";
+import AdminNav from "../../components/AdminNav";
 import SideBar from "../../components/Sidebar";
 
 const Products = () => {
-  return <SideBar />;
+  return (
+    <>
+      <SideBar />
+      <AdminNav />
+      <section className="ml-64 bg-gray-900 min-h-screen pt-28 px-4">
+        <div className="bg-gray-800 text-white p-4">
+          hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Products;
