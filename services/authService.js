6d3f32b5a9e7eea1252a58443e9132f5189c5a4d
Tuhsarshrivastav <ts3657@gmail.com>
2022-02-14 13:50:13 +0530

    added services in commits

--- services/authService.js
+++ services/authService.js
@@ -1,11 +1,14 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
+// hashed password
 module.exports.hashedPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   const hashed = await bcrypt.hash(password, salt);
   return hashed;
 };
 
+// jsonwebtoken
 module.exports.jsonwebtoken = (user) => {
   const token = jwt.sign({ user }, process.env.jwtSecret, {
     expiresIn: "7d",
@@ -13,6 +16,7 @@ module.exports.jsonwebtoken = (user) => {
   return token;
 };
 
+// comparePassword
 module.exports.comparePassword = async (password, dbPassword) => {
   return await bcrypt.compare(password, dbPassword);
 };
