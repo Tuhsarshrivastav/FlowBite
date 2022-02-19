d85f55b425662fe11d7a383107aee997b7fcad1d
Tuhsarshrivastav <ts3657@gmail.com>
2022-02-19 17:52:00 +0530

    added create category api

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,23 @@
-class Category{
-    async create(req,res){
-
+const { validationResult } = require("express-validator");
+const CategoryModel = require("../models/categorysModel");
+class Category {
+  async create(req, res) {
+    const errors = validationResult(req);
+    const { name } = req.body;
+    if (errors.isEmpty()) {
+      const exist = await CategoryModel.findOne({ name });
+      if (!exist) {
+        await CategoryModel.create({ name });
+        return res.status(201).json({ message: "category has created" });
+      } else {
+        return res
+          .status(401)
+          .json({ errors: [{ msg: `${name} is already exists` }] });
+      }
+    } else {
+      return res.status(401).json({ errors: errors.array() });
     }
+  }
 }
 
-module.exports = new Category
+module.exports = new Category();
---
+++ models/categorysModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: "String",
+    required: true,
+    unique: true,
+  },
+});
+module.exports = mongoose.model("categorie", categorySchema);
