f46fb3b05c50415f350f17fd75c953fb8fd4a02e
Tuhsarshrivastav <ts3657@gmail.com>
2022-02-23 17:08:56 +0530

    added createCateory services

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/authReducer";
 import authService from "./Services/authService";
+import categoryService from "./Services/categoryService";
 const Store = configureStore({
   reducer: {
     [authService.reducerPath]: authService.reducer,
+    [categoryService.reducerPath]:categoryService.reducer,
     authReducer: authReducer,
   },
 });
