fe6bed21ef7cc81feb447a9a09199b63f97f54db
Tuhsarshrivastav <ts3657@gmail.com>
2022-02-23 17:30:55 +0530

    added createCategory feature

--- client/src/screens/auth/AdminLogin.js
+++ client/src/screens/auth/AdminLogin.js
@@ -40,7 +40,7 @@ const AdminLogin = () => {
         {errors.length > 0 &&
           errors.map((error, key) => (
             <div key={key}>
-              <p className="bg-red-100 text-red-700 p-3 mb-2 rounded-sm text-sm font-medium">
+              <p className="alert-danger">
                 {error.msg}
               </p>
             </div>
--- client/src/screens/dashboard/CreateCategories.js
+++ client/src/screens/dashboard/CreateCategories.js
@@ -1,35 +1,55 @@
-import e from "cors";
-import {useState} from "react"
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Screenheader from "../../components/Screenheader";
 import Wrapper from "./Wrapper";
-
+import { useCreateMutation } from "../../redux/Services/categoryService";
 const CreateCategories = () => {
-  const [state,setState] =useState('')
-
-  const submitCategory = ()=>{
+  const [state, setState] = useState("");
+  const navigate = useNavigate();
+  const [saveCategory, data] = useCreateMutation();
+  const errors = data?.error?.data.errors ? data?.error?.data.errors : [];
+  console.log(data);
+  const submitCategory = (e) => {
     e.preventDefault();
-  }
+    saveCategory({ name: state });
+  };
+
+  useEffect(() => {
+    if (data?.isSuccess) {
+      navigate("/dashboard/categories");
+    }
+  }, [data?.isSuccess]);
   return (
     <Wrapper>
       <Screenheader>
         <Link to="/dashboard/categories" className="btn-dark">
-          <i class="bi bi-arrow-left-short"></i> categories list
+          <i className="bi bi-arrow-left-short"></i> categories list
         </Link>
       </Screenheader>
       <form onSubmit={submitCategory} className="w-full md:w-8/12">
         <h3 className="text-lg capitalize mb-3">create category</h3>
+        {errors.length > 0 &&
+          errors.map((error, key) => (
+            <p className="alert-danger" key={key}>
+              {error.msg}
+            </p>
+          ))}
         <div className="mb-3">
           <input
             type="text"
-            value={state.category}onChange={(e)=>setState(e.target.value)}
+            value={state.category}
+            onChange={(e) => setState(e.target.value)}
             name=""
             className="form-control"
             placeholder="Category Name..."
           />
         </div>
         <div className="mb-3">
-          <input type="submit" value="create category" class="btn-indigo" />
+          <input
+            type="submit"
+            value={data.isLoading ? "loading..." : "create category"}
+            className="btn-indigo"
+          />
         </div>
       </form>
     </Wrapper>
