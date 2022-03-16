d8f5b70767753f47458e5a4ef4eb54ac0070ca06
Tuhsarshrivastav <ts3657@gmail.com>
2022-03-16 18:38:23 +0530

    added pagination feature

--- client/src/screens/dashboard/Categories.js
+++ client/src/screens/dashboard/Categories.js
@@ -6,12 +6,16 @@ import { clearMessage } from "../../redux/reducers/globalReducer";
 import { useEffect } from "react";
 import { useGetQuery } from "../../redux/Services/categoryService";
 import Spinner from "../../components/Spinner";
-
+import Pagination from "../../components/Pagination";
 const Categories = () => {
   const dispatch = useDispatch();
-  const { page } = useParams();
+  let { page } = useParams();
+  if(!page) {
+    page = 1;
+ }
   const { success } = useSelector((state) => state.globalReducer);
-  const { data = [], isLoading } = useGetQuery(page ? page : 1);
+  const { data = [], isFetching } = useGetQuery(page ? page : 1);
+  
   useEffect(() => {
     return () => {
       dispatch(clearMessage());
@@ -26,40 +30,48 @@ const Categories = () => {
         </Link>
       </ScreenHeader>
       {success && <div className="alert-success">{success}</div>}
-      {!isLoading ? (
+      {!isFetching ? (
         data?.categories?.length > 0 && (
-          <div>
-            <table className="w-full bg-gray-900 rounded-md">
-              <thead>
-                <tr className="border-b border-gray-800 text-left">
-                  <th className="p-3 uppercase text-sm font-medium text-gray-500">
-                    name
-                  </th>
-                  <th className="p-3 uppercase text-sm font-medium text-gray-500">
-                    edit
-                  </th>
-                  <th className="p-3 uppercase text-sm font-medium text-gray-500">
-                    delete
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {data?.categories?.map((category) => (
-                  <tr key={category._id} className="odd:bg-gray-800">
-                    <td className="p-3 capitalize text-sm font-normal text-gray-400">
-                      {category.name}
-                    </td>
-                    <td className="p-3 capitalize text-sm font-normal text-gray-400">
-                      <button>edit</button>
-                    </td>
-                    <td className="p-3 capitalize text-sm font-normal text-gray-400">
-                      <button>delete</button>
-                    </td>
+          <>
+            <div>
+              <table className="w-full bg-gray-900 rounded-md">
+                <thead>
+                  <tr className="border-b border-gray-800 text-left">
+                    <th className="p-3 uppercase text-sm font-medium text-gray-500">
+                      name
+                    </th>
+                    <th className="p-3 uppercase text-sm font-medium text-gray-500">
+                      edit
+                    </th>
+                    <th className="p-3 uppercase text-sm font-medium text-gray-500">
+                      delete
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                </thead>
+                <tbody>
+                  {data?.categories?.map((category) => (
+                    <tr key={category._id} className="odd:bg-gray-800">
+                      <td className="p-3 capitalize text-sm font-normal text-gray-400">
+                        {category.name}
+                      </td>
+                      <td className="p-3 capitalize text-sm font-normal text-gray-400">
+                        <button>edit</button>
+                      </td>
+                      <td className="p-3 capitalize text-sm font-normal text-gray-400">
+                        <button>delete</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <Pagination
+              page={parseInt(page)}
+              perPage={data.perPage}
+              count={data.count}
+              path="dashboard/categories"
+            />
+          </>
         )
       ) : (
         <Spinner />
